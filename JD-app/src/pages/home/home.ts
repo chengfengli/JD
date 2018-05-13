8c97425cf4b6b91c6fef4c23d24ce4a115086382
李明 <672486796@qq.com>
2018-05-13 17:58:03 +0800

    首页

--- JD-app/src/pages/home/home.ts
+++ JD-app/src/pages/home/home.ts
@@ -6,9 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+    porducts=[];
+    porducts_width="0px";
 
-  constructor(public navCtrl: NavController) {
-
-  }
+    constructor(public navCtrl: NavController) {
+        for(let i=0;i<10;i++){
+            this.porducts.push({name:'商品'+(i+1),img:'../../assets/imgs/0'+i+'.jpg'});
+        }
+        this.porducts_width = this.porducts.length*75+'px';
+    }
 
 }
